7e27121fd5220f6478aac4b1d76c62dbc72ee54e
HliasUniwa <128928176+HliasUniwa@users.noreply.github.com>
2023-08-24 01:32:20 +0300

    CleverGame

---
+++ TeachGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Serialize its coMmand that you can chage the values on INSPECTOR!!
+    //[SerializeField]float xValue = 0;
+    [SerializeField]float yValue = 0.01f;
+    //[SerializeField]float zValue = 0;
+    [SerializeField]float moveSpeed = 2; // MoveSpeed for the input of movement 
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions(){
+        Debug.Log("Wolcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Dont hit the walls");
+    }
+
+    void MovePlayer(){
+        //transform.Translate(xValue,yValue,zValue); //we put f after the value to show that it is a float
+        //Timedelta sychronizes with frame
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
+        //with getaxis we can have control of input manager movements
+    }
+
+}
---
+++ TeachGame/Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+    [SerializeField] Animator myDoor = null;
+
+    Collider collider;
+    [SerializeField] bool openTrigger = false;
+    [SerializeField] bool closeTrigger = false;
+    [SerializeField] string OpenName = "DoorOpen";
+    [SerializeField] string CloseName = "CloceDoor";
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(openTrigger)
+            {
+                myDoor.Play(OpenName,0,0.0f);
+                //gameObject.SetActive(false);
+                //collider.enabled = true;
+            }
+            if(closeTrigger)
+            {
+                //collider.enabled = true;
+                myDoor.Play(CloseName,0,0.0f);
+                //gameObject.SetActive(false);
+            }
+        }
+    }
+}
