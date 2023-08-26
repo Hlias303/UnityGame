edba3ed5e027ff79beb833f6ec32029751a9f3b9
HliasUniwa <128928176+HliasUniwa@users.noreply.github.com>
2023-08-26 19:09:09 +0300

    ADD QUIZ(almost)

--- TeachGame/Assets/Scripts/NPCInteract.cs
+++ TeachGame/Assets/Scripts/NPCInteract.cs
@@ -1,11 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
 
 public class NPCInteract : MonoBehaviour
 {
+    public Canvas interactCanvas;
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+    public Text QuestionTxt;
+
+    public void generateQuestion()
+    {
+        currentQuestion = Random.Range(0,QnA.Count);
+
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        SetAnswers();
+    }
+
+    void SetAnswers()
+    {
+        for(int i = 0 ; i <options.Length; i++ )
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text =QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer ==i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            } 
+        }
+    }
+
+    public void correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion(); 
+    }
     public void Interact()
     {
         Debug.Log("Interact");
+        interactCanvas.GetComponent<Canvas>().enabled = true;
+        generateQuestion();
     }
 }
---
+++ TeachGame/Assets/Scripts/QuestionsAndAnswers.cs
@@ -0,0 +1,19 @@
+﻿
+[System.Serializable]
+public class QuestionsAndAnswers
+{
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
