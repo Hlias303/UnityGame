b80562167d08ee6f41c15fb6577d578bd71d92c5
Viss <kelesidisvissarion@gmail.com>
2023-08-28 19:12:15 +0300

    asdf

---
+++ TeachGame/Assets/Scripts/Keypad.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+
+    public Canvas interactCanvas;
+    [SerializeField] private Text Ans;
+    [SerializeField] private Animator Door;
+    [SerializeField] string OpenName = "DoorOpen";
+    [SerializeField]int Time_to_wait = 3;
+    private string Answer = "123";
+
+    public void Number(int number)
+    {
+        if (Ans.text == "Incorrect!")
+        {
+            Ans.color = Color.black;
+            Ans.text = "";
+            Ans.text += number.ToString();
+        }
+        else if (Ans.text == "Correct!")
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Ans.text += number.ToString();
+        }
+    }
+
+    public void Execute()
+    {
+        if (Ans.text == Answer)
+        {
+            Ans.text = "Correct!";
+            Ans.color = Color.green;
+            if(Time.time > Time_to_wait )
+            {
+                Door.Play(OpenName,0,0.0f);
+            }
+            interactCanvas.GetComponent<Canvas>().enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Ans.text = "Incorrect!";
+            Ans.color = Color.red;
+        }
+    }
+
+    public void StartKeypad()
+    {
+        Debug.Log("Started Keypad");
+        interactCanvas.GetComponent<Canvas>().enabled = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
--- TeachGame/Assets/Scripts/Player/PlayerInteract.cs
+++ TeachGame/Assets/Scripts/Player/PlayerInteract.cs
@@ -13,6 +13,12 @@ public class PlayerInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        InteractNPC();
+        InteractDoor();
+    }
+
+    void InteractNPC()
+    {
         if (Input.GetKeyDown(KeyCode.E))
         {   
             float interactRange = 2f;
@@ -22,6 +28,23 @@ public class PlayerInteract : MonoBehaviour
                 if(collider.TryGetComponent(out NPCInteract NPC))
                 {
                     NPC.Interact();
+                }
+            }   
+        }
+    }
+
+    void InteractDoor()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            float interactRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position,interactRange);
+            foreach(Collider collider in colliderArray)
+            {
+                Debug.Log("Collided with door ");
+                if(collider.TryGetComponent(out Keypad KP))
+                {
+                    KP.StartKeypad();
                 }
             }   
         }
